bfb32de8dea29992901cb4ed102d6971b4e9d3e3
Paco <fco.linares.s@gmail.com>
2023-10-02 15:28:34 -0600

    contador en el checkbox

--- app/proyecto/Items.tsx
+++ app/proyecto/Items.tsx
@@ -1,7 +1,19 @@
-import React  from 'react';
+import React from 'react';
 
-export default function item(props){ 
-  return(    
-        <li  key={props.item.id}><input type="checkbox"/>{props.item.name}</li>            
+var cont = 0;
+export default function item(props){   
+  const handleChange = () => {                   
+        if (event.target.checked) {
+          cont++         
+          console.log('true ',cont);
+        } else {
+          cont--          
+          console.log('false ',cont);
+        }        
+  }; 
+  return(           
+        <li key={props.item.id}><input type="checkbox"  onChange={handleChange}/>
+        {props.item.name}</li>                    
+      
     );
 }
--- app/proyecto/tareas.tsx
+++ app/proyecto/tareas.tsx
@@ -5,10 +5,12 @@ export default function Listado(props){
   var contador = props.Listado.length
   return(        
      <ul style={{color:'red'}}>
-          <h1 >Total tareas agregadas:{contador} </h1>   
+         <h1 >Total tareas agregadas:{contador} </h1>          
           {props.Listado.map(tarea => (          
-          <Items item={tarea}/>
-        ))}
+          <Items item={tarea}/>          
+        ))}        
+        <h1 >Total tareas seleccionadas: </h1>
       </ul>    
+      
     );
 }
